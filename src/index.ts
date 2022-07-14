f4993a15171704963ceec6fa22b088c89e2d74c2
Justin Sun <i@jsun969.cn>
2022-07-14 17:23:05 +0800

    feat: export apis and types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from './types';
+
+export { createDialogs } from './createDialogs';
+
+export { useDialog } from './useDialog';
+export { useDialogController } from './useDialogController';
+
+export { DialogProvider } from './DialogProvider';
+export { DialogController } from './DialogController';

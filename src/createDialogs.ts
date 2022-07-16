6f75124cabc03d4b078f264ec9bc266ac1a7cb6f
Justin Sun <i@jsun969.cn>
2022-07-16 18:46:55 +0800

    fix(typo): rename `defaultPropValues` to `defaultProps`

--- src/createDialogs.ts
+++ src/createDialogs.ts
@@ -4,9 +4,9 @@ export const createDialogs = <
   TDialogProps extends DialogProps,
   TNames extends string,
 >(
-  defaultPropValues: Record<TNames, TDialogProps>,
+  defaultProps: Record<TNames, TDialogProps>,
 ): Record<TNames, { isOpen: boolean } & TDialogProps> => {
-  return Object.entries(defaultPropValues).reduce((prev, [name, props]) => {
+  return Object.entries(defaultProps).reduce((prev, [name, props]) => {
     prev[name] = { ...(props as any), isOpen: false };
     return prev;
   }, {} as any);

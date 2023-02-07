f8b26704dc20d16c2346ea3c34d18ea1a839c8fa
Justin Sun <i@jsun969.cn>
2023-02-07 22:33:54 +0800

    chore(eslint): update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,15 +20,11 @@ module.exports = {
   },
   ignorePatterns: ['*.cjs', 'dist/*'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { ignoreRestSiblings: true },
-    ],
-    '@typescript-eslint/consistent-type-imports': 'error',
-    'no-console': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'no-console': 'warn',
   },
 };
